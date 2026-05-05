eb18ea043d6f4eb929d51278ab0783ab1f2e9c42
Luong Doan <147630201+ruadepzai@users.noreply.github.com>
2026-05-05 15:58:02 +0700

    Update web.php

--- order-api/routes/web.php
+++ order-api/routes/web.php
@@ -9,5 +9,9 @@ Route::get('/', function () {
 });
 
 // Các Routes chính thức cho giao diện Web (Task B.4)
-Route::get('/orders', [OrderWebController::class, 'index']);
-Route::get('/orders/{id}', [OrderWebController::class, 'show']);
+Route::get('/orders', [OrderWebController::class, 'index'])->name('orders.index');
+Route::get('/orders/{id}', [OrderWebController::class, 'show'])->name('orders.show');
+Route::get('/orders/{id}/edit', [OrderWebController::class, 'edit'])->name('orders.edit');
+Route::delete('/orders/{id}', [OrderWebController::class, 'destroy'])->name('orders.destroy');
+Route::patch('/orders/{id}/confirm', [OrderWebController::class, 'confirm'])->name('orders.confirm');
+Route::patch('/orders/{id}/cancel', [OrderWebController::class, 'cancel'])->name('orders.cancel');
